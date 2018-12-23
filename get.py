6884f0fb0fffe38e9e6a55c13130919ec4b1eecf
ZihongLin <1046298030@qq.com>
2018-12-23 22:49:26 +0800

    feature: place limit on # of answers

--- get.py
+++ get.py
@@ -2,6 +2,8 @@ import json
 import urllib.request
 
 qid = input('Please enter question id:')
+limit = int(input('Please enter the limit on the number of answers, if any (if none, put 0):'))
+i = 0
 init_req_url = "https://www.zhihu.com/api/v4/questions/{}/answers?include=content&limit=20&offset=0&platform=desktop&sort_by=default".format(qid)
 result = open("results/Q{}.md".format(qid),'w')
 response = urllib.request.urlopen(init_req_url).read()
@@ -9,7 +11,7 @@ parsed = json.loads(response)
 
 result.write('# '+parsed['data'][0]['question']['title']+'\n\n');
 
-while parsed['paging']['is_end'] == False:
+while parsed['paging']['is_end'] == False and (limit == 0 or 20*i < limit):
 	for answer in parsed['data']:
 		content = answer['content']
 		author_name = answer['author']['name']
@@ -18,5 +20,6 @@ while parsed['paging']['is_end'] == False:
 	response = urllib.request.urlopen(parsed['paging']['next']).read()
 	parsed = json.loads(response)
 	print("make 20/{}".format(parsed['paging']['totals']))
+	i += 1
 
 result.close()
